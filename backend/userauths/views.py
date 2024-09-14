01f065a60f385f286cf81408f7911142dedde6ca
MachariaP <walburphinehas78@gmail.com>
2024-09-14 16:48:04 +0300

    Implement JWT authentication

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+from userauths.models import User, Profile
+from userauths.serializer import RegisterSerializer, myTokenObtainPairSerializer
+
 # Create your views here.
+class myTokenObtainPairView(TokenObtainPairView):
+    serializer_class = myTokenObtainPairSerializer
